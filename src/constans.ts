2c29a59d5e38266ad3d0310f033216cbf9889cd6
hbn <hoang.bach.nguyen@ppi.de>
2020-11-25 15:28:37 +0100

    First commit with: gitignore, package.json, tsconfig and some testspecs about how protractor resolve promise

---
+++ src/constans.ts
@@ -0,0 +1,15 @@
+export class Constans {
+
+  public static readonly webUrlABCNews = "https://abcnews.go.com/";
+  public static readonly webTitle = "ABC News – Breaking News, Latest News, Headlines & Videos";
+
+  //For Example Angular-Webapp:
+  public static readonly exampleAngularUrl = "http://juliemr.github.io/protractor-demo/";
+  public static readonly inputField1 = "first";
+  public static readonly inputField2 = "second";
+  public static readonly goButton = "gobutton";
+  public static readonly dropDownElems = "operator";
+  public static readonly dropDownTagname = "option";
+
+
+}
